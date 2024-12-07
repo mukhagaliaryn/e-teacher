ada3bb6df0b6867882040f93cddf6bdd3e5454c6
mukhagaliaryn <mukhagaliaryn@gmail.com>
2024-12-07 12:21:58 +0500

    Done

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,10 +6,18 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.SignUpView.as_view(), name='signup'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(
+    path('password-reset/', auth_views.PasswordResetView.as_view(
         template_name='accounts/password_reset_form.html'), name='password_reset'),
-    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset_done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-
+    path('password-reset-done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='accounts/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='accounts/password_reset_confirm.html'
+    ), name='password_reset_confirm'),
+    path('reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='accounts/password_reset_complete.html'
+    ), name='password_reset_complete'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('profile/edit/', views.ProfileUpdateView.as_view(), name='edit_profile'),
+    path('profile/delete/', views.ProfileDeleteView.as_view(), name='delete_profile'),
+    path('profile/password_change/', views.CustomPasswordChangeView.as_view(), name='password_change'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,10 +4,65 @@ from django.urls import reverse_lazy
 from django.views.generic import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
+from accounts.forms import UserForm, ProfileUpdateForm
+from django.contrib.auth.models import User
 
 
+# Signup
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserForm
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
+
+
+# Profile
+class ProfileView(LoginRequiredMixin, DetailView):
+    model = User
+    template_name = 'accounts/profile.html'
+    context_object_name = 'user_profile'
+
+    def get_object(self):
+        return self.request.user
+
+
+# Edit profile
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    model = User
+    form_class = ProfileUpdateForm
+    template_name = 'accounts/edit_profile.html'
+    success_url = reverse_lazy('profile')
+
+    def get_object(self):
+        return self.request.user
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Сіздің профиліңіз сәтті жаңартылды!')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Төмендегі қателерді түзетіңіз.')
+        return super().form_invalid(form)
+
+
+# Delete profile
+class ProfileDeleteView(LoginRequiredMixin, DeleteView):
+    model = User
+    template_name = 'accounts/delete_profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
+
+
+# Password change
+class CustomPasswordChangeView(PasswordChangeView):
+    template_name = 'accounts/password_change.html'
+    success_url = reverse_lazy('profile')
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Сіздің пароліңіз сәтті жаңартылды!')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Төмендегі қатені түзетіңіз.')
+        return super().form_invalid(form)
